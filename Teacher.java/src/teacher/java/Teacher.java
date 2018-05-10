e92fc254581d8b7ecada12716e06f30da37fb305
xuechengqi3 <1581598973@qq.com>
2018-05-10 16:35:34 +0800

    老师

---
+++ Teacher.java/src/teacher/java/Teacher.java
@@ -0,0 +1,14 @@
+package teacher.java;
+
+public class Teacher {
+	String name;
+	int age;
+	String teachingcourse;
+	String ofschoolage;
+
+	public void show() {
+		System.out.println(name + "\n" + age + "\n" + teachingcourse + "\n" + ofschoolage + "\n");
+
+	}
+
+}
---
+++ Teacher.java/src/teacher/java/TeacherText.java
@@ -0,0 +1,13 @@
+package teacher.java;
+
+public class TeacherText {
+	public static void main(String[] args) {
+		Teacher teacher = new Teacher();
+		teacher.name = "宋老师";
+		teacher.age = 42;
+		teacher.teachingcourse = "java";
+		teacher.ofschoolage = "12年";
+		teacher.show();
+	}
+
+}
